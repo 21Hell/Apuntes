82670c23021c8c2c2f2acf8082d0669de1bae3dc
21Hell <l20140919@queretaro.tecnm.mx>
2022-11-25 22:32:16 -0600

    Recorrer Listas, Break y Continue

--- Platzi/Python/break_continue.py
+++ Platzi/Python/break_continue.py
@@ -1,12 +1,11 @@
 def run():
-    # for contador in range(1000):
-    #     if contador % 2 != 0:
-    #         continue
-    #     print(contador)
-    for i in range(10000):
-        print(i)
-        if i == 5678:
+    while True:
+        numero = int(input('Escribe un número: '))
+        if numero < 0:
             break
+    print(f'El cuadrado de {numero} es {numero**2}')
+
+    
 
 if __name__ == '__main__':
     run()
